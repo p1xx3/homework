695bf1f97a12d849088cd2484195b1d7c965ac02
P1xx <P1xxyxe@gmail.com>
2024-11-05 00:45:20 +0700

    Add files via upload

---
+++ maket--3/js/main.js
@@ -0,0 +1,19 @@
+(function(){
+    document.addEventListener('click',burgerInit)
+
+    function burgerInit(e){
+        const burgerInit = e.target.closest('burger-icon')
+        const burgerNavLink = e.target.closest('burger__link')
+
+
+        if(!burgerInit && !burgerNavLink ) return
+        if(document.documentElement.clientWidth >900) return
+
+        if(!document.body.classList.contains('body--opened-menu')){
+            document.body.classList.add('body--opened-menu')
+        }else{
+            document.body.classList.remove('body--opened-menu')
+        }
+    }
+})()
+
