146f771b0645372d671e3c903c9109e63d1b13c9
P1xx <P1xxyxe@gmail.com>
2024-11-20 15:36:21 +0700

    Add files via upload

--- maket--3/js/main.js
+++ maket--3/js/main.js
@@ -1,36 +1,93 @@
-(function(){
-    document.addEventListener('click',burgerInit)
+(function () {
+    // бургер 
+    document.addEventListener('click', burgerInit)
 
-    function burgerInit(e){
+    function burgerInit(e) {
         const burgerInit = e.target.closest('.burger-icon')
         const burgerNavLink = e.target.closest('.nav__link')
 
-        if(!burgerInit && !burgerNavLink ) return
-        if(document.documentElement.clientWidth >900) return
+        if (!burgerInit && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
 
-        if(!document.body.classList.contains('body--opened-menu')){
+        if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu')
-        }else{
+        } else {
             document.body.classList.remove('body--opened-menu')
         }
     }
-const open = document.querySelector('.about__img-button');
-const modal = document.querySelector('.modal');
+    // модалка 
+    const open = document.querySelector('.about__img-button');
+    const modal = document.querySelector('.modal');
 
-open.addEventListener('click', openModal)
-modal.addEventListener('click', closeModal)
+    open.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
 
-function openModal(e){
-    e.preventDefault()
-    document.body.classList.toggle('body-opened--modal')
-}
-function closeModal(e){
-    e.preventDefault()
-    const target = e.target;
-    if(target.closest('.modal__consel') || target.classList.contains('modal')){
-    document.body.classList.remove('body-opened--modal')
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body-opened--modal')
     }
-}
+    function closeModal(e) {
+        e.preventDefault()
+        const target = e.target;
+        if (target.closest('.modal__consel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body-opened--modal')
+        }
+    }
+    //  Табы
+    const tabControls = document.querySelector('.tab-controls')
 
+    tabControls.addEventListener('click', toggletab )
+
+    function toggletab(e){
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if(!tabControl) return
+        e.preventDefault()
+        if(tabControl.classList.contains('tab-controls__link--active')) return 
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControls = document.querySelector('.tab-controls__link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        if(activeContent){
+            activeContent.classList.remove('tab-content--show')
+        }
+        if(activeControls){
+            activeControls.classList.remove('tab-controls__link--active') 
+        }
+        
+        tabContent.classList.add('tab-content--show')
+        tabControl.classList.add('tab-controls__link--active')
+    }
+    // аккардион  
+    const accordionLists = document.querySelectorAll('.accordion-list');
+
+    accordionLists.forEach(el => {
+
+        el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+
+        });
+    });
 })()
 
