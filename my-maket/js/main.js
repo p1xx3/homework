6a2883c24a906f146495548ea9d72bc3c1cc5a40
P1xx <P1xxyxe@gmail.com>
2024-11-28 00:49:17 +0700

    Add files via upload

---
+++ my-maket/js/main.js
@@ -0,0 +1,33 @@
+(function () {
+    // Модалка
+    const open = document.querySelector('.sing-up');
+    const modal = document.querySelector('.modal');
+
+    open.addEventListener('click', openModal);
+    modal.addEventListener('click', closeModal);
+
+    function openModal(e) {
+        e.preventDefault();
+        document.body.classList.toggle('body-opened--modal');
+    }
+
+    function closeModal(e) {
+        e.preventDefault();
+        const target = e.target;
+        if (target.closest('.modal__closed') || target.classList.contains('modal')) {
+            document.body.classList.remove('body-opened--modal');
+        }
+    }
+
+    // Меню языков
+    const languageNav = document.querySelector('.language-nav');
+    const dropContentOpen = languageNav.parentElement;
+
+    document.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('language-nav')) {
+            dropContentOpen.classList.toggle('language-active');
+        } else {
+            dropContentOpen.classList.remove('language-active');
+        }
+    });
+})();
